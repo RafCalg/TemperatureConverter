f8a8c06d2b1152765da621ca4f3c1f4c3647bde8
Rafik Belhaouas <rafikb66@yahoo.com>
2019-10-27 12:46:20 -0600

    Add project files.

---
+++ TemperatureConverter/Converter.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Converter.aspx.cs" Inherits="TemperatureConverter.Converter" %>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: x-large;
+        }
+        .auto-style2 {
+            width: 193px;
+            height: 416px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="auto-style1">
+            <strong>Temperature Converter<br />
+            </strong>
+        </div>
+        <div>
+            <p>
+                <strong><img align="left" alt="Thermometer" class="auto-style2"  src="Image/Thermometer.jpg" /></strong></p>
+            <p>
+&nbsp; <strong>
+                <asp:Label ID="fromLabel" runat="server" Text="From"></asp:Label>
+                :</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <strong>
+                <asp:Label ID="toLabel" runat="server" Text="To:"></asp:Label>
+                </strong>&nbsp;&nbsp;&nbsp;
+            </p>
+            <p>
+                <asp:DropDownList ID="fahrCels1DropDownList" runat="server" Height="18px" OnSelectedIndexChanged="fahrCels1DropDownList_SelectedIndexChanged">
+                    <asp:ListItem Value="0">Fahrenheit</asp:ListItem>
+                    <asp:ListItem Value="1">Celsius</asp:ListItem>
+                </asp:DropDownList>
+&nbsp;&nbsp;
+                <asp:DropDownList ID="fahrCels2DropDownList"  runat="server" OnSelectedIndexChanged="fahrCels2DropDownList_SelectedIndexChanged">
+                    <asp:ListItem Value="0">Fahrenheit</asp:ListItem>
+                    <asp:ListItem Value="1">Celsius</asp:ListItem>
+                </asp:DropDownList>
+            </p>
+            <p>
+                &nbsp;</p>
+            <p>
+                <asp:Label ID="inpTempLabel" runat="server" Text="Input Temperature"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:TextBox ID="inpTempTexBox" runat="server" OnTextChanged="inpTempTexBox_TextChanged"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;
+                <asp:Label ID="messageLabel" runat="server" ForeColor="#CC0000"></asp:Label>
+            </p>
+            <p>
+                <asp:Label ID="outputTempLabel" runat="server" Text="Output Temperature"></asp:Label>
+&nbsp;
+                <asp:TextBox ID="outputTempTexBox" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                &nbsp;</p>
+            <p>
+                <asp:Button ID="convertButton" runat="server" OnClick="convertButton_Click" Text="Convert" Width="80px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="clearButton" runat="server" Text="Clear" Width="80px" OnClick="clearButton_Click" />
+            </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ TemperatureConverter/Converter.aspx.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TemperatureConverter
+{
+    public partial class Converter : System.Web.UI.Page
+    {
+        private double convertedTemp;
+        private bool haveValidTemp = false;
+        private double inpTemp;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Sets the dropdown lists to Fahrenheit and Celsius only for the first time
+            if (!IsPostBack)
+            {
+                fahrCels1DropDownList.SelectedIndex = 1;
+                fahrCels2DropDownList.SelectedIndex = 0;
+            }
+        }
+
+        protected void convertButton_Click(object sender, EventArgs e)
+        {
+            string badFieldName = null;
+            string adviceString = null;
+            //text fields to set error message
+            if (!haveValidTemp)
+            {
+                badFieldName = "Input Temperature";
+                adviceString = "Out of Range";
+            }
+
+            //if temperature input is out of range a message is displayed  and the process is interrupted
+            if (badFieldName != null)
+            {
+                messageLabel.Text = "Invalid " +badFieldName + ".\n" + adviceString;
+                return;
+            }
+
+            //if the input is correct the conversion takes place 
+          
+            //Read dropdown list selection
+            int unit1 = fahrCels1DropDownList.SelectedIndex;
+            int unit2 = fahrCels2DropDownList.SelectedIndex;
+            
+            //Fahrenheit to Fahrenheit or Celsius to Celsius
+            if ((unit1 == 0 && unit2 == 0)|(unit1 == 1 && unit2 == 1))
+            {
+                convertedTemp = inpTemp;
+            }
+            else if (unit1 == 0 && unit2 == 1) //Fahrenheit to Celsius
+            {
+                convertedTemp = (inpTemp - 32) * 1.8; 
+            }
+            else if (unit1 == 1 && unit2 == 0) //Celsius to Fahrenheit
+            {
+                convertedTemp = (inpTemp * 1.8) + 32;
+            }
+            else
+            {
+                //There is an error somewhere
+                
+            }
+            //Display of the result 
+            outputTempTexBox.Text = convertedTemp.ToString("F2");
+        }
+
+        protected void fahrCels1DropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void fahrCels2DropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void clearButton_Click(object sender, EventArgs e)
+        {
+            //Reset fields after clear button is triggered
+            inpTempTexBox.Text = "";
+            outputTempTexBox.Text = "";
+
+            messageLabel.Text = "";
+            fahrCels1DropDownList.SelectedIndex = 1;
+            fahrCels2DropDownList.SelectedIndex = 0;
+        }
+
+        protected void inpTempTexBox_TextChanged(object sender, EventArgs e)
+        {
+            //Checks whether input temperature is within bounds 
+            haveValidTemp = false;
+
+            if (double.TryParse(inpTempTexBox.Text.Trim(), out inpTemp))
+            {
+                if ((inpTemp >= -1000) && (inpTemp <= 1000))
+                {
+                    haveValidTemp = true;
+                }
+            }
+        }
+    }
+}
